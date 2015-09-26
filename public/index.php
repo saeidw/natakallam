1f1a97075d675c2daf0e16fd3ccef6ce3e0b7c09
Saeid Al-Wazzan <saeid.wazzan@nonsingular.net>
2015-09-26 11:29:09 +0300

    Use silex and Twig

--- public/index.php
+++ public/index.php
@@ -1,3 +1,14 @@
 <?php
-phpinfo();
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+$templateLoader = new Twig_Loader_Filesystem(__DIR__ . '/../src/templates');
+$twig = new Twig_Environment($templateLoader, array());
+
+$app->get('/', function () use ($app, $twig) {
+    return $twig->render('index.html', array());
+});
+
+$app->run();
 
